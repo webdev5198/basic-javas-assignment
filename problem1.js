6b9aa82f30be77a6364ecd0c76fdfb07e52fffdf
webdev5198 <tanzim615@gmail.com>
2023-10-02 13:27:14 +0600

    added a new problem

---
+++ problem1.js
@@ -0,0 +1,15 @@
+function cubeNumber(number) {
+    if (typeof number == "string") {
+        return "Invalid input";
+    }
+    else{
+        number = number*number*number;
+    }
+   
+    return number;
+    
+}
+
+const value = 3;
+const cube = cubeNumber(value);  
+console.log(cube);
